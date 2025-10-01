9e19317e19dd62022342ccf46dcae1699aa06246
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-02 00:00:45 +0530

    find maximum number

---
+++ day-1/find_max.py
@@ -0,0 +1,12 @@
+# Taking input from the user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Finding the maximum
+if num1 > num2:
+    max_num = num1
+else:
+    max_num = num2
+
+# Printing the result
+print("The maximum number is:", max_num)
