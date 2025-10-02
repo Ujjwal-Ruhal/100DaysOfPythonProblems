a18710ecc0e23fea399f6f53446c1a626e788d0e
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-02 17:41:23 +0530

    add two number example

---
+++ day-2/add_two_numbers.py
@@ -0,0 +1,12 @@
+# Direct Print with Values
+print(10+10)
+
+# Using Variables
+x = 10
+y = 5
+print(x+y)
+
+# Using User Input
+a = int(input("Enter your num: "))
+b = int(input("Enter your num: "))
+print(a+b)
