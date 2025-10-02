a2ae5fe76bcd7cbd54922072cbb5f3d02bf2b640
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-02 19:30:54 +0530

    Fahrenheit  to Celsius  conversion

---
+++ day-2/fahrenheit_to_celsius.py
@@ -0,0 +1,11 @@
+# Program to convert Fahrenheit temperature to Celsius
+
+# Taking input from the user (temperature in Fahrenheit)
+fahrenheit = float(input("Enter the value in Fahrenheit: "))
+
+# Formula to convert Fahrenheit to Celsius
+# Celsius = (Fahrenheit - 32) * 5/9
+celsius = (fahrenheit - 32) * 5/9
+
+# Displaying the result
+print("Celsius is:", celsius)
