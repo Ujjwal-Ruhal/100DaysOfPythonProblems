d807d58602dd4eeab37f9d31ffa400cb8a21d546
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-02 17:40:02 +0530

    examples of variables

---
+++ day-1/variables.py
@@ -0,0 +1,68 @@
+# Assigning value 1 to variable x
+x = 1
+print(x)   # Output: 1
+
+# Assigning value 3 to variable y
+y = 3
+print(y)   # Output: 3
+
+# Assigning value 5 to variable z
+z = 5
+print(z)   # Output: 5
+
+# Adding variables x, y, and z
+a = x + y + z
+print(a)   # Output: 9
+
+# Deleting variable a
+del a
+# print(a)   # This will give an error: NameError: name 'a' is not defined
+
+# Multiplication of two numbers
+a = 2
+b = 3
+print(a * b)   # Output: 6
+
+# Storing multiplication in variable c
+c = a * b
+print(c)   # Output: 6
+
+# Case-sensitive variable example
+age = 22
+# print(Age)   # This will give an error: NameError: name 'Age' is not defined
+
+# Increment Decrement examples
+# Assign 1 to variable d
+d = 1
+# Increment d by 1 (d = d + 1)
+d = d + 1
+print(d)   # Output: 2
+
+# Assign 2 to variable f
+f = 2
+# Increment f by 1 using shortcut operator
+f += 1
+print(f)   # Output: 3
+
+# Assign 4 to variable g
+g = 4
+# Decrement g by 2 using shortcut operator
+g -= 2
+print(g)   # Output: 2
+
+# Assign 5 to variable n
+n = 5
+# Multiply n by 2 using shortcut operator
+n *= 2
+print(n)   # Output: 10
+
+# Assign 6 to variable s
+s = 6
+# Divide s by 2 using shortcut operator
+s /= 2
+print(s)   # Output: 3.0
+
+# String concatenation
+z = 'Thank'
+z += ' You'
+print(z)   # Output: Thank You
