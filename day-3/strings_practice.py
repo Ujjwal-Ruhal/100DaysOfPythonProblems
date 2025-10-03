5e7f9021615da54c6b68b64b9aac3a6a8f9d9f5c
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-03 23:22:39 +0530

    practice of string

---
+++ day-3/strings_practice.py
@@ -0,0 +1,17 @@
+# Q1: Concatenate first name and last name
+first_name = "Priya"
+last_name = "Sharma"
+print("Full name is", first_name + " " + last_name)
+
+# Q2: Repeat a word 5 times
+word = "Namaste"
+print(word * 5)
+
+# Q3: Print first and last character of INDIA
+country = "INDIA"
+print("First:", country[0])
+print("Last:", country[-1])
+
+# Q4: Slice PYTHON to get PYO
+text = "PYTHON"
+print(text[0:5:2])
