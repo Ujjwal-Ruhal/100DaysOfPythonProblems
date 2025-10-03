aa434a3f06dc6e6d1bc1588ff3e879fd026fd654
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-03 23:27:06 +0530

    practice of some other problems

---
+++ day-3/numbers_and_conditions.py
@@ -0,0 +1,32 @@
+# Example 1: Simple addition and multiplication
+a = 10
+b = 5
+print("Addition:", a + b)   # Output: 15
+print("Multiplication:", a * b)   # Output: 50
+
+
+# Example 2: If-else condition
+marks = 72
+if marks >= 33:
+    print("Pass")
+else:
+    print("Fail")
+# Output: Pass
+
+
+# Example 3: Using input with if-else
+number = int(input("Enter a number: "))
+if number % 2 == 0:
+    print("Even number")
+else:
+    print("Odd number")
+# Example Input: 7
+# Output: Odd number
+
+
+# Example 4: Find maximum of three numbers
+x = 15
+y = 25
+z = 10
+print("Maximum is", max(x, y, z))
+# Output: Maximum is 25
