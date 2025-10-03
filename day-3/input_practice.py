551bdb4871a2d0f2d062aa76c80f78b5a3084c09
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-03 23:22:02 +0530

    practice of input

---
+++ day-3/input_practice.py
@@ -0,0 +1,13 @@
+# Q1: Take your name as input
+name = input("Enter your name: ")
+print("Hello", name)
+
+# Q2: Take two numbers and print their sum
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+print("The sum is", num1 + num2)
+
+# Q3: Take favourite food and city as input
+food = input("Enter your favourite food: ")
+city = input("Enter your favourite city: ")
+print("I love", food, "from", city)
