94b8243212c205ac1a82e0d335326c8c51be1d24
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-03 23:21:19 +0530

    practice of variables

---
+++ day-3/variables_practice.py
@@ -0,0 +1,13 @@
+# Q1: Create a variable student_name and print "My name is <name>"
+student_name = "Ramesh"
+print("My name is", student_name)
+
+# Q2: Store your age in a variable and print it
+age = 21
+print("My age is", age)
+
+# Q3: Store marks in 3 subjects and print total
+math = 85
+science = 90
+english = 80
+print("Total marks =", math + science + english)
