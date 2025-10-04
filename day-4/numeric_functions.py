0611ed389d91d943b5213f789805e71b68310666
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-04 21:30:43 +0530

    numeric functions

---
+++ day-4/numeric_functions.py
@@ -0,0 +1,16 @@
+
+# --- Built-in Numeric Functions ---
+numbers = [1, 5, 3, 76, 8, 95, 4]
+
+print("Maximum value:", max(numbers))
+print("Minimum value:", min(44, 7, 2, 4444445, 87, 5, 3))
+print("Absolute value of -454:", abs(-454))
+print("Power of 4^3:", pow(4, 3))
+
+# --- Numeric Function: round() ---
+num = 12.56789
+print(f"\nOriginal number: {num}")
+print(f"Rounded (no decimals): {round(num)}")
+print(f"Rounded (2 decimals): {round(num, 2)}")
+
+print("\n Program complete!")
