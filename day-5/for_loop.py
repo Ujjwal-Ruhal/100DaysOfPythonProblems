152556eb644d16ad78f29624de95f6de91573bad
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-10 13:46:36 +0530

    for_loop

---
+++ day-5/for_loop.py
@@ -0,0 +1,22 @@
+# use of for loop is : for example to print something to 100 time's so you dont copy paste for 100 time's so solve this type of problem's to solve using loop's
+# for loop means : you diclair a condition so for loop check the condition and execute the code for if the condition are not false
+
+#  for example :
+
+l = ['apple',2,1021,'A',10.23]
+# to see all data 
+print(l)
+
+# print all value's using for loop
+for i in l:
+    print(i)
+    ''' output : apple
+                 2
+                 1021
+                 A
+                 10.23
+                '''
+
+# one more example
+for x in ['hello','ji','kiya','haal','hai','aapke']:
+    print(x)
