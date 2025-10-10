3310d8022a070451b4d7ed37f909158eee788ba1
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-10 15:07:09 +0530

    reuse the code

--- day-5/code_reusing_using_functions.py
+++ day-5/code_reusing_using_functions.py
@@ -1,10 +1,15 @@
-'''
-code-reusing:
+# Function that prints whatever fruit name user enters
+def fruit_name():
+    name = input("Enter a fruit name: ")
+    print("You entered:", name)
 
-create-function:
-def function-name():
-    statement
+# Function call
+fruit_name()
 
-function-call:
-function-name()
-'''
+# Function with a parameter
+def greet(name):
+    print("Hello", name)
+
+# Function call with argument
+greet("Ujjwal")
+
