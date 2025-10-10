714e5077569adb07cf0f77d98c276ac698e50014
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-10 14:52:22 +0530

    update the file

--- day-5/for_loop.py
+++ day-5/for_loop.py
@@ -1,36 +1,57 @@
-# use of for loop is : for example to print something to 100 time's so you dont copy paste for 100 time's so solve this type of problem's to solve using loop's
-# for loop means : you diclair a condition so for loop check the condition and execute the code for if the condition are not false
+# -------------------------------
+# FOR LOOP EXAMPLES
+# -------------------------------
 
-#  for example :
+# Use of for loop:
+# If you want to repeat a task multiple times (like printing something 100 times),
+# instead of copy-pasting, you use a loop.
+# A for loop executes code for each item in a sequence or for a range of numbers.
 
-l = ['apple',2,1021,'A',10.23]
-# to see all data 
+# Example 1: Loop through a list of mixed data
+l = ['apple', 2, 1021, 'A', 10.23]
+
+# Print the entire list
 print(l)
 
-# print all value's using for loop
+# Print all values in the list using a for loop
 for i in l:
     print(i)
-    ''' output : apple
-                 2
-                 1021
-                 A
-                 10.23
-                '''
+    '''
+    Output:
+    apple
+    2
+    1021
+    A
+    10.23
+    '''
 
-# one more example
-for x in ['hello','ji','kiya','haal','hai','aapke']:
+# Example 2: Loop through another list
+for x in ['hello', 'ji', 'kiya', 'haal', 'hai', 'aapke']:
     print(x)
 
-# one more example like - print a table
-for i in range(2,21,2):
+# Example 3: Print a table of even numbers from 2 to 20
+for i in range(2, 21, 2):
     print(i)
-# one more example like - print a table as a user input
-n = int(input('Enter a number : '))
-for i in range(1,11):
-    num = i*n
-    print(num,'\n')
+
+# Example 4: Print a multiplication table for a user input number
+n = int(input('Enter a number: '))
+for i in range(1, 11):  # loop from 1 to 10
+    num = i * n
+    print(num, '\n')
 
 print('\n')
-# example of you print a shomething value as many time 
-for i in range(50):
+
+# Example 5: Print a message multiple times
+for i in range(50):  # loop runs 50 times
     print('I Love You')
+
+# Get the number for which the table will be printed
+n = int(input("Enter a number to print its multiplication table: "))
+
+# Get how many multiples you want
+limit = 10
+
+# Loop from 1 to limit
+for i in range(1, limit + 1):
+    result = n * i
+    print(f"{n} x {i} = {result}")
