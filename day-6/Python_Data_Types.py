ba800a4bae6524fefffcd32e625aeab6f7576a47
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-14 10:57:00 +0530

    add on examples

--- day-6/Python_Data_Types.py
+++ day-6/Python_Data_Types.py
@@ -148,3 +148,13 @@ print("Set with the use of List: ", s2)
 Access Set Items
 Set items cannot be accessed by referring to an index, since sets are unordered the items have no index. But we can loop through the set items using a for loop, or ask if a specified value is present in a set, by using the keyword in.
 '''
+set1 = set(["Hello", "For", "Hello"]) #Duplicates are removed automatically
+print(set1) 
+
+# loop through set
+for i in set1:
+   print(i, end=" ") #prints elements one by one
+  
+# check if item exist in set   
+print("Hello" in set1)
+
