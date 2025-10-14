6960d198ca6475ede854fe03c6fe2c663634d184
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-14 10:51:53 +0530

    add on examples

--- day-6/Python_Data_Types.py
+++ day-6/Python_Data_Types.py
@@ -82,3 +82,12 @@ Access List Items
 
 In order to access the list items refer to index number. In Python, negative sequence indexes represent positions from end of the array. Instead of having to compute offset as in List[len(List)-3], it is enough to just write List[-3]. Negative indexing means beginning from end, -1 refers to last item, -2 refers to second-last item, etc.
 '''
+a = ["Hello", "For", "Hello"]
+print("Accessing element from the list")
+print(a[0])
+print(a[2])
+
+print("Accessing element using negative indexing")
+print(a[-1])
+print(a[-3])
+
