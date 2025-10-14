95ff23c352d2b0e71fb12a27d2fb66e212c98ae7
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-14 10:55:00 +0530

    Set Data Type in Python

--- day-6/Python_Data_Types.py
+++ day-6/Python_Data_Types.py
@@ -128,3 +128,10 @@ print(type(True))
 print(type(False))
 print(type(true))
 
+'''
+Set Data Type in Python
+In Python Data Types, Set is an unordered collection of data types that is iterable, mutable, and has no duplicate elements. The order of elements in a set is undefined though it may consist of various elements.
+
+Create a Set in Python
+Sets can be created by using the built-in set() function with an iterable object or a sequence by placing the sequence inside curly braces, separated by a ‘comma’. The type of elements in a set need not be the same, various mixed-up data type values can also be passed to the set.
+'''
