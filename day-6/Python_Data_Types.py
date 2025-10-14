90184b8e6d15b501e6547723415cf7fe34cb4be0
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-14 10:47:26 +0530

    Numeric Data Types

--- day-6/Python_Data_Types.py
+++ day-6/Python_Data_Types.py
@@ -22,3 +22,19 @@ x = ["geeks", "for", "geeks"]  # list
 x = ("geeks", "for", "geeks")  # tuple
 x = {1:24 , 2:'hello', 'name':'ujjwal','age':20.1} # dict- key:value
 
+'''Numeric Data Types
+Python numbers represent data that has a numeric value. A numeric value can be an integer, a floating number or even a complex number. These values are defined as int, float and complex classes.
+
+Integers: value is represented by int class. It contains positive or negative whole numbers (without fractions or decimals). There is no limit to how long an integer value can be.
+Float: value is represented by float class. It is a real number with a floating-point representation. It is specified by a decimal point. Optionally, character e or E followed by a positive or negative integer may be appended to specify scientific notation.
+Complex Numbers: It is represented by a complex class. It is specified as (real part) + (imaginary part)j. For example - 2+3j
+'''
+a = 5
+print(type(a))
+
+b = 5.0
+print(type(b))
+
+c = 2 + 4j
+print(type(c))
+
