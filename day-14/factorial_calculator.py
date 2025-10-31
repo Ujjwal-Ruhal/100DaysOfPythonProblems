ff2b2f20ca1908c20d65505937a98b4965f5ae53
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-10-31 23:10:44 +0530

    Added factorial calculator program with step-by-step multiplication visualization

---
+++ day-14/factorial_calculator.py
@@ -0,0 +1,16 @@
+num = int(input("Enter a number: "))
+
+if num < 0:
+    print("Factorial not defined for negative number")
+else:
+    factorial = 1
+    steps = []
+
+    for i in range(num, 0, -1):
+        factorial *= i
+        steps.append(str(i))
+
+    if num == 0:
+        print("0! = 1")
+    else:
+        print(" x ".join(steps), "=", factorial)
