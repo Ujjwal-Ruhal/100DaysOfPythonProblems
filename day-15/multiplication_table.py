061cc04ace32ea70a794a0a1b79b4b5cb8984118
Ujjwal-Ruhal <ruhalujjwal@gmail.com>
2025-11-02 13:06:36 +0530

    Added multiplication table program with input validation and error handling

--- day-15/multiplication_table.py
+++ day-15/multiplication_table.py
@@ -4,7 +4,8 @@ try:
     if num > 0:
         for i in range(1,11):
             print(f"{num} X {i} = {num * i}")
-    
+    elif num == 0:
+        print("Multiplication table of 0 is always 0")
     else:
         print("Negative Number, Please enter a positive number")
 
